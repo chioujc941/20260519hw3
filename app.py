2d8eaee7ecda1253a029837bb991fdbfd9d58c91
chioujc941 <chioujc941@gmail.com>
2026-05-19 20:59:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with l:
     n1= st.number_input(
      "行程開始前幾分鐘提醒？",
      min_value=0, max_value=60,
-     value=15
+     value=30
     )
     if st.button("新增行程"):
         st.session_state.mylist.append(f"行程主旨:{t1},日期選擇:{t3},時間選擇:{t4},幾分鐘前提醒:{n1}")
